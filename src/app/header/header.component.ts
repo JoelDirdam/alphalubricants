5bb210e3c2a74d78f18b22c5c73bdc95504271bf
Joel Madrid <joel@advantedigital.com>
2024-06-21 22:56:38 -0600

    Products 90%

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
 
+  ngAfterViewInit() {
+    const menuButton = document.getElementById('menu-button');
+    const closeButton = document.getElementById('close-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const searchInputField = document.getElementById('search-input-field');
+    const searchInputIcon = document.getElementById('search-input-icon');
+
+    // print the variables to the console
+    console.log(searchInputField, searchInputIcon);
+
+    menuButton?.addEventListener('click', () => {
+      mobileMenu?.classList.remove('hidden');
+      searchInputField?.classList.add('hidden-input');
+      searchInputIcon?.classList.add('hidden-input');
+    });
+
+    closeButton?.addEventListener('click', () => {
+      mobileMenu?.classList.add('hidden');
+      searchInputField?.classList.remove('hidden-input');
+      searchInputIcon?.classList.remove('hidden-input');
+    });
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,10 +10,14 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductsComponent {
   products = [
-    { name: 'Alphahydraulic 300', image: 'assets/alphahydraulic300.png' },
-    { name: 'SAE 20W50 API CH-4', image: 'assets/sae20w50.png' },
-    { name: 'SAE 15W40 API CH-4', image: 'assets/sae15w40.png' },
-    { name: 'Monogrado SAE 50', image: 'assets/monogradosae50.png' },
-    { name: 'ALPHA MAXDUTY 15W40 API CI-4', image: 'assets/alphamaxduty.png' },
+    { name: 'Alphahydraulic 300', image: 'img/products/alphahydraulic300.png', url: 'https://alphalubricants.com.mx/alphahydraulic300.pdf' },
+    { name: 'SAE 20W50 API CH-4', image: 'img/products/sae20w50.png', url: 'https://alphalubricants.com.mx/sae20w50.pdf' },
+    { name: 'SAE 15W40 API CH-4', image: 'img/products/sae15w40.png', url: 'https://alphalubricants.com.mx/sae15w40.pdf' },
+    { name: 'Monogrado SAE 50', image: 'img/products/monogradosae50.png', url: 'https://alphalubricants.com.mx/monogradosae50.pdf' },
+    { name: 'ALPHA MAXDUTY 15W40 API CI-4', image: 'img/products/alphamaxduty.png', url: 'https://alphalubricants.com.mx/alphamaxduty.pdf'},
   ];
+
+  openPDF(url: string) {
+    window.open(url, '_blank');
+  }
 }
